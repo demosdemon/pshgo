5530f4b88be0c1a178df0f45ae100a322e1a529a
Brandon LeBlanc <brandon@platform.sh>
2019-05-20 20:35:54 -0500

    GetApplication

--- cmd/serve/routes/env/env.go
+++ cmd/serve/routes/env/env.go
@@ -10,10 +10,20 @@ import (
 func init() {
 	server.RegisterConfigurator("/env", func(g lars.IRouteGroup) {
 		g.Get("", GetEnv)
+		g.Get("/application", GetApplication)
 	})
 }
 
 func GetEnv(c *server.Context) {
 	env := pshgo.CloneProvider(c).(pshgo.MapProvider)
 	_ = c.JSON(200, env)
+}
+
+func GetApplication(c *server.Context) {
+	app := c.GetApplication()
+	if app == nil {
+		_ = c.Text(400, "Not Found")
+	} else {
+		_ = c.JSON(200, app)
+	}
 }
