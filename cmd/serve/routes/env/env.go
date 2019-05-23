9a0ac7b9c4b1accec6189f446fae3af2093d3db1
Brandon LeBlanc <brandon@platform.sh>
2019-05-22 19:50:41 -0500

    GET /env/export

--- cmd/serve/routes/env/env.go
+++ cmd/serve/routes/env/env.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"github.com/go-playground/lars"
+	"github.com/joho/godotenv"
 
 	"github.com/demosdemon/pshgo"
 	"github.com/demosdemon/pshgo/cmd/serve/server"
@@ -10,6 +11,7 @@ import (
 func init() {
 	server.RegisterConfigurator("/env", func(g lars.IRouteGroup) {
 		g.Get("", GetEnv)
+		g.Get("/export", GetExport)
 		g.Get("/application", GetApplication)
 	})
 }
@@ -17,6 +19,16 @@ func init() {
 func GetEnv(c *server.Context) error {
 	env := pshgo.CloneProvider(c).(pshgo.MapProvider)
 	return c.JSON(200, env)
+}
+
+func GetExport(c *server.Context) error {
+	env := pshgo.CloneProvider(c).(pshgo.MapProvider)
+	marshaled, err := godotenv.Marshal(env)
+	if err != nil {
+		return err
+	}
+
+	return c.Text(200, marshaled)
 }
 
 func GetApplication(c *server.Context) error {
