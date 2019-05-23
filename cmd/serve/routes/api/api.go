9667a7615f718df9b9d505c381f19ee40d94c1ec
Brandon LeBlanc <brandon@platform.sh>
2019-05-22 19:31:42 -0500

    refactor redirect logic

--- cmd/serve/routes/api/api.go
+++ cmd/serve/routes/api/api.go
@@ -109,10 +109,10 @@ func Redirect(c *server.Context) error {
 		return err
 	}
 
-	if n == 1 {
+	n--
+	if n == 0 {
 		http.Redirect(c.Response(), c.Request(), "/api/get", http.StatusFound)
 	} else {
-		n--
 		http.Redirect(c.Response(), c.Request(), fmt.Sprintf("/api/redirect/%d", n), http.StatusFound)
 	}
 
